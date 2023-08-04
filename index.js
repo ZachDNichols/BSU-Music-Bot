b136dabbf4bbe104f53aabaee352b2e198bb5b53
Zach Nichols <zachdnichols@gmail.com>
2023-08-04 17:47:19 -0500

    Fixed some linter issues

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 // Create a new client instance
@@ -7,10 +7,10 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
-    console.log(`Ready! Logged in as ${c.user.tag}`);
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 // Log in to Discord with your client's token
-//client.login(process.env.DISCORD_TOKEN);
+// client.login(process.env.DISCORD_TOKEN);
 
 scraper = require('./scraper.js');
--- scraper.js
+++ scraper.js
@@ -1,27 +1,26 @@
 const puppet = require('puppeteer');
-require("dotenv").config();
+require('dotenv').config();
 
-async function scrape()
-{
-    const browser = await puppet.launch({headless: false});
-    let page = await browser.newPage();
-    await page.goto('https://bemidjistate.learn.minnstate.edu/d2l/lms/news/main.d2l?ou=4779556', {waitUntil: 'networkidle0'});
-    let nextPageButton = await page.$('div[class="d2l-page-main d2l-max-width"] > div[class="d2l-page-main-padding"] > div > d2l-html-block >>> div[class=" d2l-html-block-rendered "] > p[style="text-align: center;"] > a');
-    await nextPageButton.click();
-    await page.waitForNavigation({waitUntil: 'networkidle0'});
-    let username = await page.$('#username');
-    await username.type(process.env.STAR_ID);
-    let password = await page.$('#password');
-    await password.type(process.env.D2L_PASS);
-    let login = await page.$('div[class="form-element-wrapper"] > button');
-    await login.click();
-    await page.waitForNavigation({waitUntil: 'networkidle0'});
-    await page.reload();
-    let announcmentHeader = await page.$$eval("#d2l_2_5_86 > ul > li:nth-child(1) > div.d2l-datalist-item-content > h3 > a", el => el.textContent);
-    console.log(announcmentHeader);
+async function scrape() {
+	const browser = await puppet.launch({ headless: false });
+	const page = await browser.newPage();
+	await page.goto('https://bemidjistate.learn.minnstate.edu/d2l/lms/news/main.d2l?ou=4779556', { waitUntil: 'networkidle0' });
+	const nextPageButton = await page.$('div[class="d2l-page-main d2l-max-width"] > div[class="d2l-page-main-padding"] > div > d2l-html-block >>> div[class=" d2l-html-block-rendered "] > p[style="text-align: center;"] > a');
+	await nextPageButton.click();
+	await page.waitForNavigation({ waitUntil: 'networkidle0' });
+	const username = await page.$('#username');
+	await username.type(process.env.STAR_ID);
+	const password = await page.$('#password');
+	await password.type(process.env.D2L_PASS);
+	const login = await page.$('div[class="form-element-wrapper"] > button');
+	await login.click();
+	await page.waitForNavigation({ waitUntil: 'networkidle0' });
+	await page.reload();
+	const announcmentHeader = await page.$$eval('#d2l_2_5_86 > ul > li:nth-child(1) > div.d2l-datalist-item-content > h3 > a', el => el.textContent);
+	console.log(announcmentHeader);
 }
 
-(async () =>{
-    await scrape();
+(async () => {
+	await scrape();
 })
 ();
