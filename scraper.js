f26406b3bce66b0de98b69b9f7ce5d8bd6248740
Zach Nichols <zachdnichols@gmail.com>
2023-08-04 19:24:14 -0500

    Added some of the path for selecting the announcement text

--- scraper.js
+++ scraper.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 async function scrape() {
 	const browser = await puppet.launch({ headless: false });
-	const page = await browser.newPage();
+	let page = await browser.newPage();
 	await page.goto('https://bemidjistate.learn.minnstate.edu/d2l/lms/news/main.d2l?ou=4779556', { waitUntil: 'networkidle0' });
 	const nextPageButton = await page.$('div[class="d2l-page-main d2l-max-width"] > div[class="d2l-page-main-padding"] > div > d2l-html-block >>> div[class=" d2l-html-block-rendered "] > p[style="text-align: center;"] > a');
 	await nextPageButton.click();
@@ -15,9 +15,14 @@ async function scrape() {
 	const login = await page.$('div[class="form-element-wrapper"] > button');
 	await login.click();
 	await page.waitForNavigation({ waitUntil: 'networkidle0' });
-	await page.reload();
-	const announcmentHeader = await page.$$eval('#d2l_2_5_86 > ul > li:nth-child(1) > div.d2l-datalist-item-content > h3 > a', el => el.textContent);
-	console.log(announcmentHeader);
+	await page.waitForNavigation({ waitUntil: 'networkidle0' });
+	await page
+		.waitForSelector('div[class="d2l-page-main d2l-max-width d2l-min-width"] > div[class="d2l-page-main-padding"] > div[class="d2l-homepage"] > div[class="homepage-container"] > div[class="homepage-row"] > div' +
+			' > div[class="homepage-col-8"] > div[class="d2l-widget d2l-tile"] > d2l-expand-collapse-content > div[class="d2l-widget-content-padding"] > div[class="d2l-placeholder d2l-placeholder-live"] > ' +
+			'div > div[class="d2l-datalist-container d2l-datalist-style1 d2l-datalist-block d2l-datalist-outdent"] > ul > li')
+		.then(() => console.log('got it'));
+	browser.close();
+	//	const annoucnemnetHeaderText = await (await announcmentHeader.getProperty('textContent')).jsonValue();
 }
 
 (async () => {
