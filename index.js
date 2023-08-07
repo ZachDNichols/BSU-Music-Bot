561203271edf12cefed6d33155ee0a2aaa2ca631
Zach Nichols <zachdnichols@gmail.com>
2023-08-07 10:29:52 -0500

    Scraping almost done
    Bot checks for last message sent, scrapes the pages, and sends the message. All that is left is formatting the text in a way to account for special characters.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, TextChannel} = require('discord.js');
+const fs = require('fs');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -8,13 +9,61 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+	postAnnouncement();
 });
 
 // Log in to Discord with your client's token
-// client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
 
 const scraper = require('./scraper.js');
 
-scraper.scrape().then((data) => {
-	console.log(data);
-});
+async function postAnnouncement() {
+	let firstTimeCall = false;
+	if (!firstTimeCall) {
+		await scrapeAndFormat().then((result) => { 	firstTimeCall = true; });
+	}
+
+	setInterval(await scrapeAndFormat, 86400000);
+}
+
+async function scrapeAndFormat()
+{
+	await scraper.scrapeD2L().then((result) => {
+		const res = result.replaceAll('<p>', '').replaceAll('</p>', '\n');
+
+		if (sendNewMessage(res))
+		{
+			sendMessage(res, process.env.TEST_CHANNEL);
+		}
+	});
+
+	console.log("Scraped and formatted at " + new Date().toLocaleString());
+}
+
+function sendMessage(message, id)
+{
+	const channel = client.channels.cache.get(id);
+	if (channel instanceof TextChannel)
+	{
+		channel.send("# Announcement\n" + message);
+	}
+}
+
+function sendNewMessage(message)
+{
+	if (!fs.existsSync("msg.txt"))
+	{
+		fs.writeFileSync("msg.txt", message);
+		return true;
+	}
+
+	const oldMessage = fs.readFileSync("msg.txt", "utf-8");
+
+	if (oldMessage === message)
+	{
+		return false;
+	}
+
+	fs.writeFileSync("msg.txt", message);
+	return true;
+}
--- scraper.js
+++ scraper.js
@@ -1,27 +1,27 @@
 const puppet = require('puppeteer');
 require('dotenv').config();
 
-async function scrape() {
+async function scrapeD2L() {
 	const browser = await puppet.launch();
 	const page = await browser.newPage();
-	await page.goto('https://bemidjistate.learn.minnstate.edu/d2l/lms/news/main.d2l?ou=4779556', { waitUntil: 'networkidle0' });
+	await page.goto('https://bemidjistate.learn.minnstate.edu/d2l/lms/news/main.d2l?ou=4779556', {waitUntil: 'networkidle0'});
 	const nextPageButton = await page.$('div[class="d2l-page-main d2l-max-width"] > div[class="d2l-page-main-padding"] > div > d2l-html-block >>> div[class=" d2l-html-block-rendered "] > p[style="text-align: center;"] > a');
 	await nextPageButton.click();
-	await page.waitForNavigation({ waitUntil: 'networkidle0' });
+	await page.waitForNavigation({waitUntil: 'networkidle0'});
 	const username = await page.$('#username');
 	await username.type(process.env.STAR_ID);
 	const password = await page.$('#password');
 	await password.type(process.env.D2L_PASS);
 	const login = await page.$('div[class="form-element-wrapper"] > button');
 	await login.click();
-	await page.waitForNavigation({ waitUntil: 'networkidle0' });
-	await page.waitForNavigation({ waitUntil: 'networkidle0' });
+	await page.waitForNavigation({waitUntil: 'networkidle0'});
+	await page.waitForNavigation({waitUntil: 'networkidle0'});
 	const body = await page.waitForSelector('div[class="d2l-page-main d2l-max-width d2l-min-width"] > div[class="d2l-page-main-padding"] > div[class="d2l-homepage"] > div[class="homepage-container"] > div[class="homepage-row"] > div' +
-			' > div[class="homepage-col-8"] > div[class="d2l-widget d2l-tile"] > d2l-expand-collapse-content > div[class="d2l-widget-content-padding"] > div[class="d2l-placeholder d2l-placeholder-live"] > ' +
-			'div > div[class="d2l-datalist-container d2l-datalist-style1 d2l-datalist-block d2l-datalist-outdent"] > ul > li:nth-child(1) > div[class="d2l-datalist-item-content"] > div > div[class="d2l-htmlblock-untrusted"] > d2l-html-block');
+		' > div[class="homepage-col-8"] > div[class="d2l-widget d2l-tile"] > d2l-expand-collapse-content > div[class="d2l-widget-content-padding"] > div[class="d2l-placeholder d2l-placeholder-live"] > ' +
+		'div > div[class="d2l-datalist-container d2l-datalist-style1 d2l-datalist-block d2l-datalist-outdent"] > ul > li:nth-child(1) > div[class="d2l-datalist-item-content"] > div > div[class="d2l-htmlblock-untrusted"] > d2l-html-block');
 	const innerText = (await body.getProperty('html')).jsonValue();
 	await browser.close();
 	return innerText;
 }
 
-module.exports = { scrape };
+module.exports = { scrapeD2L };
